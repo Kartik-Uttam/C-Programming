b7311f6fe0850f84ab897d655f6f46c38a399ca3
Kartik Uttam <kartik.uttam.dev@gmail.com>
2026-06-25 10:45:39 +0530

    Initial commit

---
+++ max_array.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/* Function Prototypes */
+void input(int n, int arr[n]);
+int find_max_index(int n, int arr[n]);
+void output(int arr[], int max_index);
+
+int main()
+{
+    
+    printf("Enter number of elements: ");
+
+    if()
+    {
+        printf("Invalid array size\n");
+        return 0;
+    }
+
+    return 0;
+}
+
+/* Function to read array elements */
+void input(int n, int arr[n])
+{
+    printf("Enter element at index %d: ", i);
+}
+
+/* Function to find index of maximum element */
+int find_max_index(int n, int arr[n])
+{
+
+}
+
+/* Function to display result */
+void output(int arr[], int max_index)
+{
+    printf("Maximum value = %d\n", );
+    printf("Index of maximum value = %d\n", );
+}
