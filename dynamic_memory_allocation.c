b8128c2a7f7b7c16a30047ba24b0b132c15e76a5
Kartik Uttam <kartik.uttam.dev@gmail.com>
2026-06-28 21:53:26 +0530

    Completed the code

--- dynamic_memory_allocation.c
+++ dynamic_memory_allocation.c
@@ -9,51 +9,61 @@ void delete_array(int **arr);
 
 int main()
 {
-    
+    int n;
     printf("Enter number of elements: ");
-    
-    printf("Invalid size\n");
-
+    scanf("%d",&n);
+    if(n<=0)
+    {
+        printf("Invalid size\n");
+        return 0;
+    }
     /* Allocate memory */
-
-    printf("Memory allocation failed\n");
-
+    int *arr=create_array(n);
+    if(arr==NULL)
+    {
+        printf("Memory allocation failed\n");
+        return 0;
+    }
 
     /* Initialize array */
-   
+   initialize_array(arr,n);
 
     /* Print array */
     printf("\nArray elements:\n");
-
+    print_array(arr,n);
 
     /* Delete array */
-
+    delete_array(&arr);
     return 0;
 }
 
 /* Function to allocate memory */
 int* create_array(int n)
 {
-
+ int *arr=(int*)malloc(n*sizeof(int));
 }
 
 /* Function to initialize array */
 void initialize_array(int *arr, int n)
 {
-
-        printf("Enter element at index %d: ", );
-
+    for(int i=0;i<n;i++)
+    {
+        printf("Enter element at index %d: ", i);
+        scanf("%d",&arr[i]);
+    }
 }
 
 /* Function to print array */
 void print_array(int *arr, int n)
 {
-
-        printf("%d ", );
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",arr[i] );
+    }
 }
-
 /* Function to delete array */
 void delete_array(int **arr)
 {
-
+    free(*arr);
+    *arr=NULL;
 }
