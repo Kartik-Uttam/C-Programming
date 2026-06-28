8980e4f3edc789ce97a2ade8382e8966d94987c2
Kartik Uttam <kartik.uttam.dev@gmail.com>
2026-06-28 21:43:30 +0530

    Code to do dynamic memory allocation .

---
+++ dynamic_memory_allocation.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Function Prototypes */
+int* create_array(int n);
+void initialize_array(int *arr, int n);
+void print_array(int *arr, int n);
+void delete_array(int **arr);
+
+int main()
+{
+    
+    printf("Enter number of elements: ");
+    
+    printf("Invalid size\n");
+
+    /* Allocate memory */
+
+    printf("Memory allocation failed\n");
+
+
+    /* Initialize array */
+   
+
+    /* Print array */
+    printf("\nArray elements:\n");
+
+
+    /* Delete array */
+
+    return 0;
+}
+
+/* Function to allocate memory */
+int* create_array(int n)
+{
+
+}
+
+/* Function to initialize array */
+void initialize_array(int *arr, int n)
+{
+
+        printf("Enter element at index %d: ", );
+
+}
+
+/* Function to print array */
+void print_array(int *arr, int n)
+{
+
+        printf("%d ", );
+}
+
+/* Function to delete array */
+void delete_array(int **arr)
+{
+
+}
